e38f0c8077e52da643f8ce6d7c2f0e82de5f5c47
Zunayed Shahriar <zunayedshahriar@gmail.com>
2023-04-12 16:29:23 +0600

    resume + minor changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Zunayed Shahriar',
     description:
       'Zunayed Shahriar is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://zunayed.net', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
